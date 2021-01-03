b8e5651807d0a4431f82cba452a081ac8501769d
Jasdeep <41632+jasdeepkhalsa@users.noreply.github.com>
2021-01-03 00:26:13 +0000

    Updated readme and code comments

---
+++ README.md
@@ -0,0 +1,11 @@
+# JSVM - JavaScript Virtual Machine
+
+Mimics a virtual von Neumann architecture with:
+
+:heavy_check_mark: `ArrayBuffer` used for Memory & Registers
+:heavy_check_mark: JavaScript Class for CPU operations like fetching memory, executing instructions
+:heavy_check_mark: Limited assembly language defined using JavaScript binary hexadecimal literals
+
+## Credits
+
+Adapted from a [lecture series](https://www.youtube.com/watch?v=fTBwD3sb5mw&list=PLP29wDx6QmW5DdwpdwHCRJsEubS5NrQ9b&index=1) by @francisrstokes
--- cpu.js
+++ cpu.js
@@ -2,6 +2,8 @@ const createMemory = require('./memory')
 const { Register } = require('./register')
 const Instruction = require('./instruction')
 
+const multiplier16Bit = 2
+
 class CPU {
   constructor(memory) {
     this.memory = memory
@@ -15,11 +17,10 @@ class CPU {
       Register.GENERAL_4,
     ]
 
-    this.registers = createMemory(this.registerNames.length * 2) // TODO: Why 2?
+    this.registers = createMemory(this.registerNames.length * multiplier16Bit)
 
     this.registersMap = this.registerNames.reduce((result, name, index) => {
-      // TODO: Why 2?
-      result[name] = index * 2
+      result[name] = index * multiplier16Bit
       return result
     }, {})
   }
@@ -39,7 +40,7 @@ class CPU {
     ).map((val) => `0x${val.toString(16).padStart(2, '0')}`)
 
     console.log(
-      `${address.toString(16).padStart(4, '0')}: ${nextEightBytes.join(' ')}`
+      `0x${address.toString(16).padStart(4, '0')}: ${nextEightBytes.join(' ')}`
     )
   }
 
@@ -66,7 +67,7 @@ class CPU {
     )
     // Get the actual instruction from memory
     const instruction = this.memory.getUint8(nextInstructionAddress)
-    // Increment the instruction pointer
+    // Increment the instruction pointer by 1
     this.setRegister(Register.INSTRUCTION_POINTER, nextInstructionAddress + 1)
     // Return the instruction
     return instruction
@@ -80,7 +81,10 @@ class CPU {
     // Get the actual instruction from memory
     const instruction = this.memory.getUint16(nextInstructionAddress)
     // Increment the instruction pointer by 2
-    this.setRegister(Register.INSTRUCTION_POINTER, nextInstructionAddress + 2)
+    this.setRegister(
+      Register.INSTRUCTION_POINTER,
+      nextInstructionAddress + multiplier16Bit
+    )
     // Return the instruction
     return instruction
   }
@@ -92,7 +96,8 @@ class CPU {
         const literal = this.fetch16BitInstruction()
         // Get encoded register index
         const register =
-          (this.fetch8BitInstruction() % this.registerNames.length) * 2
+          (this.fetch8BitInstruction() % this.registerNames.length) *
+          multiplier16Bit
         // Move literal into register
         this.registers.setUint16(register, literal)
         return
@@ -101,22 +106,26 @@ class CPU {
       case Instruction.MOVE_REGISTER_REGISTER: {
         // Get from and to registers
         const registerFrom =
-          (this.fetch8BitInstruction() % this.registerNames.length) * 2
+          (this.fetch8BitInstruction() % this.registerNames.length) *
+          multiplier16Bit
         const registerTo =
-          (this.fetch8BitInstruction() % this.registerNames.length) * 2
+          (this.fetch8BitInstruction() % this.registerNames.length) *
+          multiplier16Bit
         // Get value from register
         const value = this.registers.getUint16(registerFrom)
-        // Set value in to register
+        // Set value in "to" register
         this.registers.setInt16(registerTo, value)
         return
       }
 
       case Instruction.MOVE_REGISTER_MEMORY: {
         const registerFrom =
-          (this.fetch8BitInstruction() % this.registerNames.length) * 2
+          (this.fetch8BitInstruction() % this.registerNames.length) *
+          multiplier16Bit
         const memoryAddress = this.fetch16BitInstruction()
         // Get value from register
         const value = this.registers.getUint16(registerFrom)
+        // Set value into memory
         this.memory.setUint16(memoryAddress, value)
         return
       }
@@ -124,9 +133,11 @@ class CPU {
       case Instruction.MOVE_MEMORY_REGISTER: {
         const memoryAddress = this.fetch16BitInstruction()
         const registerTo =
-          (this.fetch8BitInstruction() % this.registerNames.length) * 2
+          (this.fetch8BitInstruction() % this.registerNames.length) *
+          multiplier16Bit
         // Get value from register
         const value = this.registers.getUint16(memoryAddress)
+        // Set value in "to" register
         this.registers.setUint16(registerTo, value)
         return
       }
@@ -135,8 +146,13 @@ class CPU {
       case Instruction.ADD_REGISTER_REGISTER: {
         const register1 = this.fetch8BitInstruction()
         const register2 = this.fetch8BitInstruction()
-        const register1Value = this.registers.getUint16(register1 * 2)
-        const register2Value = this.registers.getUint16(register2 * 2)
+        const register1Value = this.registers.getUint16(
+          register1 * multiplier16Bit
+        )
+        const register2Value = this.registers.getUint16(
+          register2 * multiplier16Bit
+        )
+        // Set value in "results" register
         this.setRegister(Register.RESULTS, register1Value + register2Value)
         return
       }
